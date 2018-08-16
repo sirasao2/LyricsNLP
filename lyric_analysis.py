cb4e0bf41f4e51f64e95005791d0aca3ed41b17f
Rahul Sirasao <rahulsirasao96@gmail.com>
2018-08-16 13:13:24 -0700

    commit with comments

--- lyric_analysis.py
+++ lyric_analysis.py
@@ -13,9 +13,9 @@ class Lyrics(object):
 	def grab_links(self, main_page):
 		"""
 		This function:
-		  - scrapes titles of songs from the metrolyrics "Popular Songs" page using bs4 and saves to array arr_post
-	      - the string contents of arr_post (which are song titles) are formatted to be lower case, punctuation free, and hyphenated
-	      - the revised strings are then concatenated to a URL template which links users to each "Popular Songs" complete lyrics
+			- scrapes titles of songs from the metrolyrics "Popular Songs" page using bs4 and saves to array: arr_post
+	    	- the string contents of arr_post (which are song titles) are formatted to be lower case, punctuation free, and hyphenated
+	    	- the revised strings are then concatenated to a URL template which links users to each "Popular Songs" complete lyrics
 		:type main_page: string
 		:rtype final_links: array 
 		"""
@@ -27,19 +27,19 @@ class Lyrics(object):
 		arr_post = [str(item).lower().replace(" ","-").replace("'", "").replace("/","") for item in arr_post]
 		
 		for i in arr_post:
-			final_links.append("http://www.metrolyrics.comet:" + i + "-drake.html")
+			final_links.append("http://www.metrolyrics.com/" + i + "-drake.html")
 		return final_links
 
 	def corpus_creation(self, final_links):
-        """
-        This function:
-            - reads/gets links and encodes to utf-8
-            - complete lyrics of each individual title are then scraped and appended to a string: lyrics
-            - all lyrics are then lower cased and stripped of punctuation in order to tidy data
-            - lyrics are saved into text file clean and ready for data analysis
-        :type final_links: array
-        :rtype drake_lyics.txt: .txt file
-        """
+		"""
+		This function:
+		    - reads/gets links and encodes to utf-8
+		    - complete lyrics of each individual title are then scraped and appended to a string: lyrics
+		    - all lyrics are then lower cased and stripped of punctuation in order to tidy data
+		    - lyrics are saved into text file clean and ready for data analysis
+		:type final_links: array
+		:rtype drake_lyics.txt: .txt file
+		"""
 		for link in final_links:
 			page = requests.get(link)
 			page.encoding = 'utf-8'
@@ -53,6 +53,13 @@ class Lyrics(object):
 			#	the_file.write(lyrics)
 	
 	def word_freq(self, file_content):	
+		"""
+		This function:
+		    - uses the tidied data to output word frequency in decreasing order
+		    - plots can be used also to display data
+		:type file_content: .txt file
+		:rtype counts: .txt file
+		"""	
 		tokens = nltk.word_tokenize(file_content)#.encode('utf8'))
 		counts = Counter(tokens)
 		#s = Series(counts)
